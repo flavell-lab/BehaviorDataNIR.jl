764ed14290a51a3a7ec31179f75493c18b7e327b
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-02-16 07:14:44 -0500

    Update stage_data.jl
    add zero_stage()

--- src/stage_data.jl
+++ src/stage_data.jl
@@ -1,3 +1,10 @@
+function zero_stage(pos_stage)
+    x, y = pos_stage[1,:], pos_stage[2,:]
+    @assert(all(.!isnan.([x[1], y[1]])))
+
+    x .- x[1], y .- y[1]
+end
+
 """
     impute_stage(x::Array{<:AbstractFloat,1})
 
