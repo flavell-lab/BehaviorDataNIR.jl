a11f856fa62cb8b7a39e92e8c1c64f1ff4a23427
jungyeonp <jungyeon@mit.edu>
2021-02-26 10:58:54 -0500

    add read_hdf5

--- src/util.jl
+++ src/util.jl
@@ -5,3 +5,11 @@ end
 function vec_ang(v1, v2)
     acos(dot(v1, v2) / (norm(v1,2) * norm(v2,2)))
 end
+
+function read_hdf5(input_h5_path::String)
+    img_nir = h5read(input_h5_path, "img_nir")
+    list_pos_stage = h5read(input_h5_path, "pos_stage")
+    pos_feature = h5read(input_h5_path, "pos_feature")
+    
+    return img_nir, list_pos_stage, pos_feature
+end
