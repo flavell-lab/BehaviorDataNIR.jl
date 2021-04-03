c5c542a9238e7aa0d106a1832a457c7e9585654d
urlicht <jungsu93@gmail.com>
2021-04-02 20:08:04 -0400

    fix Float32 typo

--- src/segmentation.jl
+++ src/segmentation.jl
@@ -1,6 +1,6 @@
 function downsample_unet_input(img)
     @assert(size(img) == (968, 732))
-    img = Flaot32.(img[4:963, 4:723])
+    img = Float32.(img[4:963, 4:723])
     (img[1:2:end-1,1:2:end-1] .+ img[2:2:end,2:2:end] .+
         img[1:2:end-1,2:2:end] .+ img[2:2:end,1:2:end-1]) ./ 4
 end
