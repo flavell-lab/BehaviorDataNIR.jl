67642c7d420af903261cf1b10270b72bba7fb0ab
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-07-06 16:19:51 -0400

    Update util.jl
    implement `write_behavior_video`

--- src/util.jl
+++ src/util.jl
@@ -89,3 +89,46 @@ function savitzky_golay_filter(data, lag; is_derivative::Bool=false, has_inflect
 end
 
 euclidean_dist(x1, x2) = norm(x1 .- x2, 2)
+
+"""
+    write_behavior_video(path_h5, path_vid=nothing; fps=20, encoder_options=nothing, downsample=true)
+Writes a video of the behavior data
+Arguments
+---------
+* `path_h5`: path of the HDF5 file  
+* `path_vid`: path of the video to be generated. if `nothing` (default), automatically generates it  
+* `fps`: frame rate of the video (default: 20 to match the acquisition rate)  
+* `downsample`: if true downsample by factor of 2   
+* `encoder_options`: in named tuple (e.g. `(crf="23", preset="slow")`)
+
+### Notes on the encoder options  
+`preset`: possible options are: ultrafast, superfast, veryfast, faster, fast, medium – default preset, slow, slower, veryslow.  
+`crf`: The range of the CRF scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible.  
+A lower value generally leads to higher quality, and a subjectively sane range is 17–28.  
+Consider 17 or 18 to be visually lossless or nearly so; it should look the same or nearly the same as the input but it isn't technically lossless.  
+"""
+function write_behavior_video(path_h5, path_vid=nothing; fps=20, encoder_options=nothing, downsample=true)
+    path_vid = isnothing(path_vid) ? splitext(path_h5)[1] * "_$(fps)fps.mp4" : path_vid
+    if splitext(path_vid)[2] !== ".mp4"
+        error("`path_vid` extension must be .mp4")
+    end
+    
+    encoder_options = (crf="23", preset="slow")
+    target_pix_fmt = VideoIO.AV_PIX_FMT_YUV420P
+    
+    h5open(path_h5, "r") do h5f
+        img_nir = h5f["img_nir"]
+        n_x, n_y, n_t = size(img_nir)
+
+        img_t1 = uint8_to_rgb(downsample ? ds(img_nir[:,:,1]) : img_nir[:,:,1])'
+        
+        open_video_out(path_vid, img_t1, framerate=20,
+            encoder_options=encoder_options, codec_name="libx264",
+            target_pix_fmt=target_pix_fmt) do vidf
+            @showprogress for t = 2:n_t
+                img_ = uint8_to_rgb(downsample ? ds(img_nir[:,:,t]) : img_nir[:,:,t])'
+            write(vidf, img_)
+            end # t
+        end # vid
+    end #h5open
+end
